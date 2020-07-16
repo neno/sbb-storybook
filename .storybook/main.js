e105dad30e44d10bc25a08f778eafa204606e256
Nenad Brcic <neno@neno.ch>
2020-07-16 15:26:21 +0200

    Add storybook-addon-html

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,5 @@
 module.exports = {
   stories: ['../src/**/*.stories.js'],
   presets: ['@storybook/preset-scss'],
+  addons: ['@whitespace/storybook-addon-html/register'],
 };
---
+++ .storybook/preview.js
@@ -0,0 +1,8 @@
+// import '../src/styles/main.scss';
+
+import { addDecorator } from '@storybook/html';
+// import { withA11y } from '@storybook/addon-a11y';
+import { withHTML } from '@whitespace/storybook-addon-html/html';
+
+// addDecorator(withA11y);
+addDecorator(withHTML);
